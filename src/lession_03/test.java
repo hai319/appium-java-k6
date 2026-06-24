4df9395aec25060b9ae3d9258c2937ac17bb3f00
haibui <hai.bui@consultinghnl.vn>
2026-06-24 17:06:33 +0700

    -n

--- src/lession_03/test.java
+++ src/lession_03/test.java
@@ -1,4 +1,22 @@
 package lession_03;
 
 public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        if (a > b) {
+            if (a > c) {
+                System.out.println("a is the largest");
+            } else {
+                System.out.println("c is the largest");
+            }
+        } else {
+            if (b > c) {
+                System.out.println("b is the largest");
+            } else {
+                System.out.println("c is the largest");
+            }
+        }
+    }
 }
